925d0d6c0a4116a50129d6eeae98e0d754b1b441
Jesse Gibson <JesseTheGibson@gmail.com>
2020-11-19 00:52:57 -0800

    Add Docusaurus template project
    This is nearly a perfect duplication of the docusaurus template, with
    the exception of a few changed links and swapped images. I still have to
    rip out the content that's there and replace it with something
    meaningful.

---
+++ docusaurus.config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  title: 'Retreon',
+  tagline: 'A toolkit for building better redux apps.',
+  url: 'https://retreon.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'retreon',
+  projectName: 'retreon',
+  themeConfig: {
+    navbar: {
+      title: 'Retreon',
+      logo: {
+        alt: 'Retreon Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/retreon/retreon',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/retreon/retreon',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Jesse Gibson`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/retreon/website/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
